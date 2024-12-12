4491ad888c290eebe32c46ee89534de144cdbf24
Code Pixelz Media <arpanadhikari63@gmail.com>
2024-12-12 15:43:56 +0545

    account setting and my offer

--- src/components/Tabs/AccountSettings.jsx
+++ src/components/Tabs/AccountSettings.jsx
@@ -10,17 +10,37 @@
 // Language/Region Settings: Choose preferred language or region for localized features.
 // Choose standard currency option global.
 
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState } from "react";
 import styles from "./Tabs.module.css"; // Import styles
-// import "./AccountSettings.css";
-import accountstyles from "./AccountSettings.css";
+import accountstyles from "./AccountSettings.module.css";
+import { FaPlus } from "react-icons/fa6";
+import { FaTimes } from "react-icons/fa";
+import { RxCross2 } from "react-icons/rx";
+import { IoCheckmarkOutline } from "react-icons/io5";
+import { VscSettings } from "react-icons/vsc";
+import { TbPasswordUser } from "react-icons/tb";
+import { Si2Fas } from "react-icons/si";
+import { GrLogin } from "react-icons/gr";
+import { IoMdNotificationsOutline } from "react-icons/io";
+import { MdLanguage } from "react-icons/md";
 
 const AccountSettings = () => {
   const [activeAccordion, setActiveAccordion] = useState(null);
   const [settingsMessage, setSettingsMessage] = useState("");
+  const [isLeaseToOwnEnabled, setLeaseToOwnEnabled] = useState(false);
+
+  // State for Notification Toggles
+  const [notificationToggles, setNotificationToggles] = useState({
+    domain: false,
+    payment: false,
+  });
 
   const handleAccordionToggle = (accordion) => {
     setActiveAccordion(activeAccordion === accordion ? null : accordion);
+  };
+
+  const handleLeaseToOwnToggle = () => {
+    setLeaseToOwnEnabled((prevState) => !prevState);
   };
 
   const handleSaveMessage = (message) => {
@@ -34,32 +54,68 @@ const AccountSettings = () => {
 
       {/* Password Management Accordion */}
       <div className={accountstyles.settingsAccoridionContainer}>
-        <div className={accountstyles.accordionItem}>
+        <div
+          className={`${accountstyles.accordionItem} ${
+            activeAccordion === "password-management" ? accountstyles.open : ""
+          }`}
+        >
           <div
             className={accountstyles.accordionHeader}
             onClick={() => handleAccordionToggle("password-management")}
           >
-            Password Management
+            <h3>
+              <TbPasswordUser />
+              Password Management
+            </h3>
+            <div className={accountstyles.svg_wrapper_bg_grey}>
+              {activeAccordion === "password-management" ? (
+                <FaTimes />
+              ) : (
+                <FaPlus />
+              )}
+            </div>
           </div>
           {activeAccordion === "password-management" && (
             <div className={accountstyles.accordionContent}>
-              <div className={accountstyles.settingItem}>
-                <h4>Two-Factor Authentication (2FA)</h4>
-                <p>Enable or disable 2FA for added security.</p>
-                <button
-                  onClick={() => handleSaveMessage("2FA settings saved.")}
+              <div
+                className={`${accountstyles.settingItem} ${accountstyles.toggle2FA}`}
+              >
+                <div>
+                  <h4>
+                    <Si2Fas />
+                    Two-Factor Authentication (2FA)
+                  </h4>
+                  <p>Enable or disable 2FA for added security.</p>
+                </div>
+
+                <div
+                  className={styles.toggle_button}
+                  onClick={handleLeaseToOwnToggle}
                 >
-                  Toggle 2FA
-                </button>
+                  <div
+                    className={`${styles.toggle_switch} ${
+                      isLeaseToOwnEnabled ? styles.on : styles.off
+                    }`}
+                  >
+                    <div className={styles.toggle_indicator}>
+                      <RxCross2 />
+                      <IoCheckmarkOutline />
+                    </div>
+                  </div>
+                </div>
               </div>
               <div className={accountstyles.settingItem}>
-                <h4>Login Activity</h4>
+                <h4>
+                  <GrLogin />
+                  Login Activity
+                </h4>
                 <p>View recent login locations and devices.</p>
-                <ul>
+                <ul className={accountstyles.loggedInDevices}>
                   <li>Device 1: New York, USA</li>
                   <li>Device 2: London, UK</li>
                 </ul>
                 <button
+                  className={styles.hover_blue_white}
                   onClick={() =>
                     handleSaveMessage("Logged out of all devices.")
                   }
@@ -72,63 +128,121 @@ const AccountSettings = () => {
         </div>
 
         {/* Preferences Accordion */}
-        <div className={accountstyles.accordionItem}>
+        <div
+          className={`${accountstyles.accordionItem} ${
+            activeAccordion === "preferences" ? accountstyles.open : ""
+          }`}
+        >
           <div
             className={accountstyles.accordionHeader}
             onClick={() => handleAccordionToggle("preferences")}
           >
-            Preferences
+            <h3>
+              <VscSettings /> Preferences
+            </h3>
+            <div className={accountstyles.svg_wrapper_bg_grey}>
+              {activeAccordion === "preferences" ? <FaTimes /> : <FaPlus />}
+            </div>
           </div>
           {activeAccordion === "preferences" && (
             <div className={accountstyles.accordionContent}>
-              <div className={accountstyles.settingItem}>
-                <h4>Notification Settings</h4>
+              {/* Notification Settings */}
+              <div
+                className={`${accountstyles.settingItem} ${accountstyles.notificationSettings}`}
+              >
+                <h4>
+                  <IoMdNotificationsOutline />
+                  Notification Settings
+                </h4>
                 <p>Customize email or SMS notifications for:</p>
                 <ul>
                   <li>
-                    <label>
-                      <input
-                        type="checkbox"
-                        onChange={() =>
-                          handleSaveMessage("Notification settings saved.")
+                    <div>
+                      <span>Domain inquiries/offers</span>
+                      <div
+                        className={styles.toggle_button}
+                        onClick={() =>
+                          setNotificationToggles((prev) => ({
+                            ...prev,
+                            domain: !prev.domain,
+                          }))
                         }
-                      />
-                      Domain inquiries/offers
-                    </label>
+                      >
+                        <div
+                          className={`${styles.toggle_switch} ${
+                            notificationToggles.domain ? styles.on : styles.off
+                          }`}
+                        >
+                          <div className={styles.toggle_indicator}>
+                            <RxCross2 />
+                            <IoCheckmarkOutline />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
                   </li>
                   <li>
-                    <label>
-                      <input
-                        type="checkbox"
-                        onChange={() =>
-                          handleSaveMessage("Notification settings saved.")
+                    <div>
+                      <span>Payment updates</span>
+                      <div
+                        className={styles.toggle_button}
+                        onClick={() =>
+                          setNotificationToggles((prev) => ({
+                            ...prev,
+                            payment: !prev.payment,
+                          }))
                         }
-                      />
-                      Payment updates
-                    </label>
+                      >
+                        <div
+                          className={`${styles.toggle_switch} ${
+                            notificationToggles.payment ? styles.on : styles.off
+                          }`}
+                        >
+                          <div className={styles.toggle_indicator}>
+                            <RxCross2 />
+                            <IoCheckmarkOutline />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
                   </li>
                 </ul>
               </div>
-              <div className={accountstyles.settingItem}>
-                <h4>Language/Region Settings</h4>
-                <p>
-                  Choose preferred language or region for localized features.
-                </p>
-                <select
-                  onChange={() => handleSaveMessage("Language settings saved.")}
-                >
-                  <option value="en">English</option>
-                  <option value="es">Spanish</option>
-                  <option value="fr">French</option>
-                </select>
-                <p>Choose standard currency option globally:</p>
-                <select
-                  onChange={() => handleSaveMessage("Currency settings saved.")}
-                >
-                  <option value="usd">USD</option>
-                  <option value="eur">EUR</option>
-                  <option value="gbp">GBP</option>
-                </select>
+
+              {/* Language/Region Settings */}
+              <div
+                className={`${accountstyles.settingItem} ${accountstyles.preferneces_settings}`}
+              >
+                <h4>
+                  <MdLanguage />
+                  Language/Region Settings
+                </h4>
+                <div>
+                  <p>
+                    Choose preferred language or region for localized features.
+                  </p>
+                  <select
+                    onChange={() =>
+                      handleSaveMessage("Language settings saved.")
+                    }
+                  >
+                    <option value="en">English</option>
+                    <option value="es">Spanish</option>
+                    <option value="fr">French</option>
+                  </select>
+                </div>
+                <div>
+                  <p>Choose standard currency option globally:</p>
+                  <select
+                    onChange={() =>
+                      handleSaveMessage("Currency settings saved.")
+                    }
+                  >
+                    <option value="usd">USD</option>
+                    <option value="eur">EUR</option>
+                    <option value="gbp">GBP</option>
+                  </select>
+                </div>
               </div>
             </div>
           )}
--- src/components/Tabs/MyOffers.jsx
+++ src/components/Tabs/MyOffers.jsx
@@ -6,6 +6,8 @@ import { ReactComponent as MyOfferIcon } from "./image/offer.svg";
 import { ReactComponent as SortIcon } from "./image/sort.svg";
 import { ReactComponent as OfferActive } from "./image/offers_active.svg";
 import { ReactComponent as OfferDecline } from "./image/offers_declined.svg";
+import { ReactComponent as CartIcon } from "./image/sales_overview.svg";
+import { ReactComponent as RemakeOfferIcon } from "./image/pricing_setup.svg";
 import { ReactComponent as DeleteIcon } from "./image/delete.svg";
 import { FaCircle, FaTimes } from "react-icons/fa"; // Import necessary icons
 import { FaPlus } from "react-icons/fa6";
@@ -49,7 +51,7 @@ const MyOffers = () => {
       <div className={styles.offer_tabs_wrapper}>
         {/* Nav tabs */}
         <div
-          className={`${styles.ws_flex} ${styles.ai_center} ${styles.justify_space_between} ${styles.ph_30} ${styles.dashboard_small_margin}`}
+          className={`${styles.ws_flex} ${styles.ai_center} ${styles.justify_space_between} ${styles.dashboard_small_margin}`}
         >
           <ul className={`${styles.nav_tabs} ${styles.offer_tabs_navs}`}>
             <li
@@ -76,7 +78,7 @@ const MyOffers = () => {
               className={`${
                 activeTab === "accepted" ? styles.active : ""
               } my_offer_accepted_tab`}
-              onClick={() => setActiveTab("accpeted")}
+              onClick={() => setActiveTab("accepted")}
             >
               {/* <img src={save_draft_icon} alt="Save Draft Icon" className={styles.tab_icon} /> */}
               <div className={styles.svg_wrapper_bg_white}>
@@ -92,16 +94,21 @@ const MyOffers = () => {
           </div>
         </div>
         {/* Tab content */}
-        <div className={styles.tab_content}>
+        <div>
           {activeTab === "active" && (
             <div className={`${styles.ws_flex} ${styles.recent_offers_cols}`}>
               {[1, 2, 3].map((item, index) => (
-                <div key={index} className={`${styles.recentOffers_wrapper} `}>
+                <div
+                  key={index}
+                  className={`${styles.recentOffers_wrapper} myOffers_wrapper `}
+                >
                   {/* Offer card */}
                   <div
                     className={`${styles.ws_flex} ${styles.gap_10} ${styles.fd_column}`}
                   >
-                    <div className={styles.recentOffers_card}>
+                    <div
+                      className={`${styles.recentOffers_card} myOffers_flex3`}
+                    >
                       <div className={styles.recentOffers_card_image}>
                         <img src={domain_img} alt="Domain" />
                       </div>
@@ -110,20 +117,26 @@ const MyOffers = () => {
                         <h5>debugbot.com</h5>
                       </div>
                       <div className={styles.recentOffers_card_details}>
-                        <p>Offer Amount</p>
+                        <p>My Offer</p>
                         <h6>$5000</h6>
                       </div>
                     </div>
-                    <div className={styles.recentOffers_card}>
-                      <div className={styles.recentOffers_card_image}>
-                        <MyOfferIcon />
+                    <div
+                      className={`${styles.recentOffers_card} myOffers_flex3`}
+                    >
+                      <div class="width_100">
+                        <div
+                          className={`${styles.recentOffers_card_image} myOffers_offer_image`}
+                        >
+                          <MyOfferIcon />
+                        </div>
                       </div>
                       <div
                         className={`${styles.recentOffers_card_titles} ${styles.offers_card_customers}`}
                       >
-                        <p className="online">
+                        <p className="expiry_soon">
                           Offer Expiry
-                          <FaCircle />
+                          <span> Soon </span>
                         </p>
                         <h5>Dec 8, 2025</h5>
                       </div>
@@ -140,7 +153,7 @@ const MyOffers = () => {
                       </div>
                     </div>
                     <div
-                      className={`${styles.recentOffers_card} ${styles.offer_status_cards}`}
+                      className={`${styles.recentOffers_card} ${styles.offer_status_cards} myOffers_flex1`}
                     >
                       {/* <div className={styles.recentOffers_card_titles}>
                         <p>Status</p>
@@ -151,7 +164,12 @@ const MyOffers = () => {
                           Pending
                         </h5>
                       </div> */}
-                      <div className={styles.recentOffers_card_details}>
+                      <div
+                        className={`${styles.recentOffers_card_details} myOffers_icons`}
+                      >
+                        <div className={styles.svg_wrapper_bg_grey}>
+                          <FiMail />
+                        </div>
                         <div
                           className={`${styles.svg_wrapper_bg_grey} ${
                             expanded[index]
@@ -164,9 +182,6 @@ const MyOffers = () => {
                           ) : (
                             <FaPlus onClick={() => toggleExpanded(index)} />
                           )}
-                        </div>
-                        <div className={styles.svg_wrapper_bg_grey}>
-                          <FiMail />
                         </div>
                       </div>
                     </div>
@@ -184,50 +199,58 @@ const MyOffers = () => {
                           <img src={domain_img}></img>
                         </div>
                         <div className={styles.recentOffers_card_titles}>
-                          <p>Product</p>
+                          <p>Select Counter Offer</p>
                           <h5>debugbot.com</h5>
                         </div>
                         <div className={styles.recentOffers_card_details}>
-                          <p>Offer Amount</p>
+                          <p>Save</p>
                           <h6>$5000</h6>
                         </div>
                       </div>
                       <form
-                        className={styles.offerForm}
+                        className={`${styles.offerForm} myOffers_extra_offer_form`}
                         onSubmit={handleSubmit}
                       >
                         <div className={styles.p_relative}>
                           <input
                             type="number"
                             className={styles.offerInput}
-                            placeholder="Enter your counter offer"
+                            placeholder="Remake an Offer"
                             min="0"
                           />
+                          <span className="remake_offer_icon">
+                            <RemakeOfferIcon />
+                          </span>
                           <button type="submit" className={styles.submitButton}>
                             <span className={styles.arrow}>&#8594;</span>{" "}
                             {/* Arrow symbol */}
                           </button>
                         </div>
-                        <div className={`${styles.ws_flex} ${styles.gap_10}`}>
-                          <button type="button" className={styles.acceptButton}>
-                            Accept
-                          </button>
+                        <div
+                          className={`${styles.ws_flex} ${styles.gap_10} myOffers_extra_column_buttons `}
+                        >
                           <button
                             type="button"
                             className={styles.declineButton}
                           >
-                            Decline
+                            <div className={`svg_white`}>
+                              <OfferDecline />
+                            </div>
+                          </button>
+                          <button type="button" className={styles.acceptButton}>
+                            <div className={`${styles.small_svg} svg_white `}>
+                              <CartIcon />
+                            </div>
                           </button>
 
                           {/* Reset button with delete icon */}
-                          <button
+                          {/* <button
                             type="button"
                             className={styles.resetButton}
                             onClick={handleReset}
                           >
-                            {/* <img src={delete_reset_icon} /> */}
                             <DeleteIcon />
-                          </button>
+                          </button> */}
                         </div>
                       </form>
                     </div>
@@ -239,6 +262,9 @@ const MyOffers = () => {
 
           {activeTab === "declined" && (
             <div>Declined offers content goes here</div>
+          )}
+          {activeTab === "accepted" && (
+            <div>Accepted offers content goes here</div>
           )}
         </div>
       </div>
